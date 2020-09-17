3d03446230cce391be0cf89ade4f05da8183b23f
Ilya Kuznetsov <ikuz2002@gmail.com>
2020-09-17 23:00:29 +0300

    Major changes

--- types.sh
+++ types.sh
@@ -2,7 +2,11 @@
 
 # [[ -z "$IMPORT_TYPE" ]] && IMPORT_TYPE=true || return 0
 
-NEW_INSTANCE_ID=0
+NEW_INSTANCE_ID=btp/NEW_INSTANCE_ID.txt
+INSTANCES=btp/INSTANCES.txt
+TYPES=btp/TYPES.txt
+INSTANCE_PROPS=btp/INSTANCE_PROPS.txt
+TEMP=btp/TEMP
 
 type_error() {
     echo "Type error: $@" > /dev/stderr
@@ -21,61 +25,102 @@ clear() {
 init() {
     clear
     mkdir btp
-    touch btp/TYPES.txt
-    touch btp/INSTANCES.txt
-    touch btp/INSTANCE_PROPS.txt
+    echo 0 > $NEW_INSTANCE_ID
+    touch $TYPES
+    touch $INSTANCES
+    touch $INSTANCE_PROPS
+}
+
+new_id() {
+    id=$(cat $NEW_INSTANCE_ID)
+    echo $id
+    let "id=$id+1"
+    echo $id > $NEW_INSTANCE_ID
+}
+
+defined() {
+    grep -xq "$1 .*" $TYPES && return 0 || return -1
+}
+
+exists() {
+    grep -xq "$1 .*" $INSTANCES && return 0 || return -1
+}
+
+propdefined() {
+    grep -xq "$1 $2" $TYPES && return 0 || return -1
 }
 
 newstruct() {
     local type=$1
-    local pattern='\$$type .\*'
-    awk '!/pattern/' btp/TYPES.txt > btp/TEMP && mv btp/TEMP btp/TYPES.txt
+    local pattern="\$$type .*"
+    awk '!/pattern/' $TYPES > $TEMP && mv $TEMP $TYPES
     for ((i=2; i <= $#; i++))
     do
 	local prop=${!i}
-	echo $type $prop >> btp/TYPES.txt
+	echo $type $prop >> $TYPES
     done
 }
 
 delete() {
     local id=$1
-    echo '!/\$$id .\*/'
-    awk '!/\$$id .\*/' btp/INSTANCES.txt > btp/TEMP && mv btp/TEMP btp/INSTANCES.txt
-    awk '!/\$$id .\*/' btp/INSTANCE_PROPS.txt > btp/TEMP && mv btp/TEMP btp/INSTANCE_PROPS.txt
+    awk '$1 != '$id' {print $0}' $INSTANCES > $TEMP && mv $TEMP $INSTANCES
+    awk '$1 != '$id' {print $0}' $INSTANCE_PROPS > $TEMP && mv $TEMP $INSTANCE_PROPS
 }
 
 new() {
     local type=$1
-    ! grep -Fxq '$type .*' btp/TYPES.txt && type_error "type $type not found"
+    ! defined $type && type_error "type $type not found"
 
-    delete $NEW_INSTANCE_ID
+    id=$(new_id)
     
-    echo $NEW_INSTANCE_ID $type >> btp/INSTANCES.txt
+    delete $id
+
+    echo $id $type >> $INSTANCES
 
     i=2
-    for prop in $( grep -Fx '$type .*' btp/TYPES.txt | awk '{print $2}' )
+    for prop in $( grep -x "$type .*" $TYPES | awk '{print $2}' )
     do
-	echo $NEW_INSTANCE_ID \'$prop ${!i}\' >> btp/INSTANCE_PROPS.txt
-	i=$(( $i + 1 ))
+      echo $id $prop \"${!i}\" >> $INSTANCE_PROPS
+      (( i++ ))
     done
 
-    echo $NEW_INSTANCE_ID
-    
-    NEW_INSTANCE_ID=$(( $NEW_INSTANCE_ID + 1 ))
+    echo $id
 }
 
 get() {
     local id=$1
     local prop=$2
-    ! grep -Fxq '$id .*' btp/INSTANCES && instance_error "instance $id not found"
-    grep -Fx '$id[:space:]+$prop .*' btp/INSTANCE_PROPS.txt | awk '{print $3}'
+    ! exists $id && instance_error "instance $id not found"
+    grep -x "^$id $prop .*" $INSTANCE_PROPS | awk '{print $3}'
+}
+
+propset() {
+    local id=$1
+    local prop=$2
+    local value=$3
+    ! exists $id && instance_error "instance $id not found"
+    ! propdefined $(typeof $id) $prop && type_error "type does not have prop $prop"
+    # TODO set prop
+}
+
+typeof() {
+    id=$1
+    ! exists $id && instance_error "instance $id not found"
+    grep "^$id .*" $INSTANCES | awk '{print $2}'
 }
 
 init
 
 newstruct person name surname age
 
-new person ILYA KUZNETSOV 18
-#user=$( new person ILYA KUZNETSOV 18 )
+user=$( new person ILYA KUZNETSOV 18 )
+user2=$( new person KIRILL TEST 20 )
 
-#get $user name
+get $user name
+get $user2 age
+
+propset $user2 surname IVANOV
+
+delete $user2
+
+typeof $user
